98770b919d744825f11021c48e88da8fe4467b73
JikanHou <62175579+JikanHou@users.noreply.github.com>
2024-11-05 16:04:37 -0500

    git ignore test

--- LandlordRecruitment/settings.py
+++ LandlordRecruitment/settings.py
@@ -1,6 +1,4 @@
-protocal = "your protocal"
-username = "your username"
-password = "your password"
-ip = "your db addr"
-SQLALCHEMY_DATABASE_URI = f"{protocal}://{username}:{password}@{id}"
+username = "t4"
+password = "0PJn7OaANQELNVx2"
+SQLALCHEMY_DATABASE_URI = f"mariadb+mariadbconnector://{username}:{password}@34.125.69.91/f24_housing_db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
