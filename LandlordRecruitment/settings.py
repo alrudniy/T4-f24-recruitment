21df2ca7766e71628d93f6cc5cfc62072abaf8d6
JikanHou <62175579+JikanHou@users.noreply.github.com>
2024-11-05 16:01:14 -0500

    Update settings.py

--- LandlordRecruitment/settings.py
+++ LandlordRecruitment/settings.py
@@ -1,4 +1,6 @@
-username = "t4"
-password = "0PJn7OaANQELNVx2"
-SQLALCHEMY_DATABASE_URI = f"mariadb+mariadbconnector://{username}:{password}@34.125.69.91/f24_housing_db"
+protocal = "your protocal"
+username = "your username"
+password = "your password"
+ip = "your db addr"
+SQLALCHEMY_DATABASE_URI = f"{protocal}://{username}:{password}@{id}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
