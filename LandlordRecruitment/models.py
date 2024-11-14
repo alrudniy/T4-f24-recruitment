48db080e6852fe2fd72c7f851a433e9bf61d9547
JikanHou <62175579+JikanHou@users.noreply.github.com>
2024-11-14 15:21:01 -0500

    API update

--- LandlordRecruitment/models.py
+++ LandlordRecruitment/models.py
@@ -1,17 +1,23 @@
 from flask_login import UserMixin
 from LandlordRecruitment import db
 from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
+
+class verification_code():
+    def verification_code(self, code):
+        self.code = code
+        self.create_time = datetime.datetime.now().timestamp()
+    
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
+    phone_number = db.Column(db.String(16), unique = True)
     email_addr = db.Column(db.String(128), unique = True)
-    #nickname = db.Column(db.String(128), unique = True)
     username = db.Column(db.String(40), unique = True)
     password_hash = db.Column(db.String(128))
-#    avatar = db.Column(db.String(128), default = "defaultAvatar.png")
-    first_mame = db.Column(db.String(40))
+    first_name = db.Column(db.String(40))
     last_name = db.Column(db.String(40))
-    is_admin = db.Column(db.Integer)
+    is_admin = db.Column(db.Integer, default = 0)
 
     def setPassword(self, password):
         self.passwordHash = generate_password_hash(password)
--- LandlordRecruitment/views.py
+++ LandlordRecruitment/views.py
@@ -1,16 +1,104 @@
-from flask import render_template, redirect, url_for, flash, abort, request, make_response
-from LandlordRecruitment.forms import LoginForm
+from flask import render_template, redirect, url_for, flash, abort, request, make_response, jsonify
+from LandlordRecruitment.models import User, verification_code
 from LandlordRecruitment import App, db
+from werkzeug.security import generate_password_hash, check_password_hash
+import flask_login
+import json
+import random
+import datetime
 
-@App.route("/")
-def index():
-    # return """<h1>123</h1>"""
-    return render_template("index.html")
+string_pool = "0123456789"
+verification_code = {}
 
-@App.route("/test")
-def test():
-    return render_template("test.html")
+@App.route("/send_code", methods = ["POST"])
+def send_code():
+    if request.method == "POST":
+        request_data = request.get_json()
+        phonenumber = request_data["phone"]
+        code = ""
+        for _ in range(6):
+            code += random.sample(string_pool, 1)
+        verification_code[phonenumber] = verification_code(code)
+        # TODO: call 3rd party api to send the code
+        return {
+            "code": 0,
+            "info": "Verification code sent"
+        }
+        
+@App.route("/login_password", methods = ["POST"])
+def login_password():
+    if request.method == "POST":
+        username = request.form["phoneNumber"]
+        password = request.form["password"]
+        user = User.query.filter(User.username == username).first()
+        if not user:
+            return {
+                "code": 1,
+                "info": "Account not exist"
+            }
+        elif not check_password_hash(user.password, password):
+            return {
+                "code": 2,
+                "info": "Password or username not correct"
+            }
+        else:
+            flask_login.login_user(user)
+            return {
+                "code": 0,
+                "info": "Login success"
+            }
+    #return render_template("login.html", form = LoginForm)
 
-@App.route("/login")
-def login():
-    return render_template("login.html", form = LoginForm)
+def check_verification_code(phone, code, expire_time = 15 * 60 * 1000):
+    db_code = verification_code.get(phone, None)
+    if not db_code:
+        return False
+    now = datetime.datetime.now().timestamp()
+    if now - db_code.create_time > expire_time:
+        return False
+    return  db_code.code == code
+        
+@App.route("/login_code", methods = ["POST"])
+def login_code():
+    if request.method == "POST":
+        data = request.get_json()
+        phone = data["phone"]
+        code = data["code"]
+        user = User.query.filter(User.phone_number == phone).first()
+        if not user:
+            return {
+                "code": 1,
+                "info": "Phone number not exist"
+            }
+        elif not check_verification_code(phone, code):
+            return {
+                "code": 2,
+                "info": "Verification code not correct"
+            }
+        else:
+            flask_login.login_user(user)
+            return {
+                "code": 0,
+                "info": "Login success"
+            }
+        
+@App.route("/register", methods = ["POST"])
+def register():
+    if request.method == "POST":
+        data = request.get_json()
+        phone = data["phone"]
+        first_name = data["firstName"]
+        last_name = data["lastName"]
+        email = data["email"]
+        new_user = User()
+        new_user.phone_number = phone
+        new_user.first_name = first_name
+        new_user.last_name = last_name
+        new_user.email_addr = email
+        new_user.is_admin = False
+        db.session.add(new_user)
+        db.session.commit()
+        return {
+            "code": 0,
+            "info": "Success"
+        }
